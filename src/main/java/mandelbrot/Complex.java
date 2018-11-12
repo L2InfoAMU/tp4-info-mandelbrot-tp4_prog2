51099b5c12c458d942572a00c962271c79508f33
t17001743 <samantha.thiel@etu.univ-amu.fr>
2018-11-12 13:39:19 +0100

    correction de la méthode negate()

--- src/main/java/mandelbrot/Complex.java
+++ src/main/java/mandelbrot/Complex.java
@@ -92,8 +92,9 @@ public class Complex {
      * @return A complex <code>c</code> such that <code>this + c = 0</code>
      */
     Complex negate() {
-        return new Complex(-this.real, this.imaginary);
+        return new Complex(-this.real, -this.imaginary);
     }
+
 
     /**
      * The conjugate of a complex number
